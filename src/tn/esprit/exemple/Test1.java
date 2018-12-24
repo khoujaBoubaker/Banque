06ea957cbe87053b2fbad10891ee6c35a8119b7a
Khouja Boubaker <khouja.boubaker@yahoo.com>
2018-12-24 15:50:46 +0100

    add second class

---
+++ src/tn/esprit/exemple/Test1.java
@@ -0,0 +1,11 @@
+package tn.esprit.exemple;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" jour apres jour");
+
+	}
+
+}
