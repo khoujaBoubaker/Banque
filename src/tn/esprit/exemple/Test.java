54f6ed8c6166a39553619264f19394dba44c8120
Khouja Boubaker <khouja.boubaker@yahoo.com>
2018-12-24 15:40:45 +0100

    add main class

---
+++ src/tn/esprit/exemple/Test.java
@@ -0,0 +1,19 @@
+package tn.esprit.exemple;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+			
+			for (int i=1;i<=5;i++){
+				System.out.println("Hello world ..."+i);
+				}
+			
+			
+		
+
+	}
+
+}
